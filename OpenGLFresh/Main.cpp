da0d6326156f6ff12e2c0e639917fe51d9fbd84e
EthanSteed <ethansteed15@gmail.com>
2020-06-23 10:58:37 +0100

    sort triangle colour plus add dynamic triangle allocation the issues with the colour code have been sorted and an index to draw mutliple triangles has been made to reduce repeating co-ordinaties

--- OpenGLFresh/Main.cpp
+++ OpenGLFresh/Main.cpp
@@ -1,11 +1,16 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 //set up for vertex shader in GLSL
 const char* vertexShaderSource = "#version 330 core\n"
@@ -16,11 +21,11 @@ const char* vertexShaderSource = "#version 330 core\n"
 "}\0";
 
 //set up fragment shader
-const char* fragmentShaderSource = "#version 330 \n"
-"out ver4 FragColor;\n"
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = ver4 (1.0F, 0.5F, 0.2F, 1.0F)\n"
+"   FragColor = vec4 (1.0F, 0.5F, 0.2F, 1.0F);\n"
 "}\n\0";
 
 int main()
@@ -31,7 +36,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -51,7 +56,7 @@ int main()
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
     //check for errors
-    int  success;
+    int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (!success)
@@ -88,25 +93,42 @@ int main()
 
     //define points to render
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f,
-         0.0f, -0.5f, 0.0f
+         0.45f,  0.0f, 0.0f,
+        -0.45f,  0.0f, 0.0f,
+         0.0f, -0.9f, 0.0f,
+         0.9f, -0.9f, 0.0f,
+        -0.9f, -0.9f, 0.0f,
+         0.0f,  0.9f, 0.0f
     };
 
+    //declare order of drawing triangles 
+    unsigned int index[] = {
+        0, 1, 2,
+        0, 2, 3,
+        1, 2, 4,
+        0, 1, 5,
+    };
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
     unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    //wireframe
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
     while (!glfwWindowShouldClose(window))
@@ -119,9 +141,13 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO); 
+        // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //draw elements contains the number of vertices to draw look into automation
+        glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_INT, 0);
 
+        
+        glBindVertexArray(0);
 
         //draw to screen
         glfwSwapBuffers(window);
@@ -131,6 +157,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
