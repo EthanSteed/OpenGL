c08fa86bbf0879150dce405d578e44d21ece8814
EthanSteed <66733107+EthanSteed@users.noreply.github.com>
2020-06-23 16:31:59 +0100

    combine index with colour with the index allowing for multiplie triangles at once easliy being combined with the colour shader

--- OpenGLFresh/Main.cpp
+++ OpenGLFresh/Main.cpp
@@ -97,14 +97,14 @@ int main()
 
     //define points to render
     float vertices[] = {
-         0.45f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.45f,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-         0.00f, -0.9f, 0.0f, 0.0f, 0.0f, 1.0f,
-//         0.90f, -0.9f, 0.0f,
-//        -0.90f, -0.9f, 0.0f,
-//         0.00f,  0.9f, 0.0f
+         0.45f,  0.0f, 0.0f, 0.5f, 0.0f, 0.5f,
+        -0.45f,  0.0f, 0.0f, 0.0f, 0.5f, 0.5f,
+         0.00f, -0.9f, 0.0f, 0.5f, 0.5f, 0.0f,
+         0.90f, -0.9f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.90f, -0.9f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.00f,  0.9f, 0.0f, 0.0f, 0.0f, 1.0f,
     };
-    /*
+
     //declare order of drawing triangles 
     unsigned int index[] = {
         0, 1, 2,
@@ -112,9 +112,9 @@ int main()
         1, 2, 4,
         0, 1, 5,
     };
-    */
-    //unsigned int EBO;
-    //glGenBuffers(1, &EBO);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
     unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
@@ -125,8 +125,8 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -149,7 +149,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         //glUseProgram(shaderProgram);
-        //glBindVertexArray(VAO); 
+        glBindVertexArray(VAO); 
         // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         
         // colour change
@@ -160,10 +160,10 @@ int main()
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         */
         //draw elements contains the number of vertices to draw look into automation
-        //glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_INT, 0);
 
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glBindVertexArray(0);
 
