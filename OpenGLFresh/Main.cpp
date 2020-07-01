39f24314f4cab282012a2edf81409a5c18875445
EthanSteed <66733107+EthanSteed@users.noreply.github.com>
2020-07-01 12:40:46 +0100

    3D cubes WIP issues with the cube formation with some faces being rendered incorrectly and making it look weird.

--- OpenGLFresh/Main.cpp
+++ OpenGLFresh/Main.cpp
@@ -115,28 +115,84 @@ int main()
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
         -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
         */
-        
+        /*
          0.45f,  0.0f, 0.0f,    0.5f, 0.0f, 0.5f,    0.75f, 0.5f, //mid right
         -0.45f,  0.0f, 0.0f,    0.0f, 0.5f, 0.5f,    0.25f, 0.5f, //mid left
          0.00f, -0.9f, 0.0f,    0.5f, 0.5f, 0.0f,    0.50f, 0.0f, //bottom mid
          0.90f, -0.9f, 0.0f,    1.0f, 0.0f, 0.0f,    1.00f, 0.0f, //bottom right
         -0.90f, -0.9f, 0.0f,    0.0f, 1.0f, 0.0f,    0.00f, 0.0f, //bottom left
          0.00f,  0.9f, 0.0f,    0.0f, 0.0f, 1.0f,    0.50f, 1.0f, //top
-         
+         */
+        -0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,     0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,    0.0f, 1.0f, 0.0f,     1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,    0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,    1.0f, 1.0f, 0.0f,     1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 1.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 1.0f,     0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,    1.0f, 0.0f, 0.0f,     0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,    0.0f, 1.0f, 0.0f,     1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 1.0f, 0.0f,     1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 1.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,     0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,     1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,     1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,    1.0f, 1.0f, 0.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,    1.0f, 0.0f, 0.0f,     1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,     1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    1.0f, 1.0f, 0.0f,     0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,    0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,    1.0f, 0.0f, 0.0f,     0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,    0.0f, 1.0f, 0.0f,     1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,    0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,    1.0f, 1.0f, 0.0f,     1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,    0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,    0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,    1.0f, 0.0f, 0.0f,     0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,    0.0f, 1.0f, 0.0f,     1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,    0.0f, 0.0f, 1.0f,     1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,    1.0f, 1.0f, 0.0f,     1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,    0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,    0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
     };
 
     //declare order of drawing triangles 
+    /*
     unsigned int index[] = {
         /*
         0,  1,  3,
         1,  2,  3,
         */
-        
+        /*
         0, 1, 2,
         0, 2, 3,
         1, 2, 4,
         0, 1, 5,
         
+    };
+    */
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
     unsigned int EBO;
@@ -151,15 +207,15 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
 
+    //Position
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
     // texture coord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
@@ -238,40 +294,54 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
         
-        float Variance = 0.1*sin((float)glfwGetTime());
-
-        //Transform
-        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        transform = glm::translate(transform, (glm::vec3(0.0f, 0.0f, 0.0f) + Variance));
-        transform = glm::scale(transform, (glm::vec3(10, 10, 10) * Variance));
-        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        
-
         //glUseProgram(shaderProgram);
-        ourShader.use();
-        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
 
         //VeiwPoints
-        glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        //glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
 
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-        unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view");
+        unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view" );
         // pass them to the shaders (3 different ways)
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
         // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
         ourShader.setMat4("projection", projection);
+        
+        glBindVertexArray(VAO);
 
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
 
-        glBindVertexArray(VAO); 
+            
+            float Variance = 0.01 * sin((float)glfwGetTime()) * i;
+
+            //Transform
+            glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+            transform = glm::translate(transform, (glm::vec3(0.0f, 0.0f, 0.0f) + Variance));
+            transform = glm::scale(transform, (glm::vec3(10, 10, 10) * Variance));
+            transform = glm::rotate(transform, (float)glfwGetTime() / i, glm::vec3(0.0f, 0.0f, 1.0f));
+
+            ourShader.use();
+            unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+            
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+ 
         // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         
         // colour change
@@ -282,7 +352,7 @@ int main()
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         */
         //draw elements contains the number of vertices to draw look into automation
-        glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_INT, 0);
+        //glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_INT, 0);
 
         //glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
