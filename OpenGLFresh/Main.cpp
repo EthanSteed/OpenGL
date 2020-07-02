098bb63164e0dd047557803278b402054499a19b
EthanSteed <66733107+EthanSteed@users.noreply.github.com>
2020-07-02 13:51:04 +0100

    Camera Control Working

--- OpenGLFresh/Main.cpp
+++ OpenGLFresh/Main.cpp
@@ -306,8 +306,13 @@ int main()
 
         
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
+        float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        ourShader.setMat4("view", view);
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view" );
@@ -324,9 +329,9 @@ int main()
 
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            float angle = (float)glfwGetTime() * 20.0f * (i+1);
+            float angle = 20.0f * (i+1);
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            model = glm::translate(model, (glm::vec3(0.0f, 0.0f, 0.0f) + sin((float)glfwGetTime())));
+            //model = glm::translate(model, (glm::vec3(0.0f, 0.0f, 0.0f) + sin((float)glfwGetTime())));
             ourShader.setMat4("model", model);
 
             
